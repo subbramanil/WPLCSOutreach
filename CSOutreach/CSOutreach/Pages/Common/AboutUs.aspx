65dd7f873f34a07496fef21d57793892bcefd925
subbramanil <sxl146830@utdallas.edu>
2014-11-08 18:48:44 -0600

    Initial Commit
    Initial Commit of entire project

---
+++ CSOutreach/CSOutreach/Pages/Common/AboutUs.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="About Us" Language="C#" MasterPageFile="../MasterPages/Site.Master" AutoEventWireup="true" CodeBehind="AboutUs.aspx.cs" Inherits="CSOutreach.AboutUs" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
+
+</asp:Content>
---
+++ CSOutreach/CSOutreach/Pages/MasterPages/Site.Master.cs
@@ -0,0 +1,53 @@
+﻿using CSOutreach.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSOutreach
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if(! Page.IsPostBack)
+           {
+               InitPage();
+           }
+        }
+
+        private void InitPage()
+        {
+            getDynamicMenuContent();
+        }
+        protected string getDynamicMenuContent()
+        {
+           // TODO: Replace this dummy content with html of the same format generated dynamically from the database.
+            string MenuContent = MenuRender.DynamicMenu;
+
+            return MenuContent;
+        }
+
+        protected string getLoginButtonText()
+        {
+            if (Session["username"] != null){
+                return "Log Out";
+            }
+            return "Log In";
+        }
+
+        protected void login_ServerClick(object sender, EventArgs e){
+            //TODO: Add real code (or redirect) to log in or log out as appropriate.
+            if (Session["username"] == null)
+            {
+                Session["username"] = "user";
+            }
+            else
+            {
+                Session["username"] = null;
+            }
+        }
+    }
+}
