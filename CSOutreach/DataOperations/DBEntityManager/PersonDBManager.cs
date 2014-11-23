68f3bae96d23d9badf245d678d2be8ce8f25b572
MayurGk <gkwdmayur@gmail.com>
2014-11-22 20:04:26 -0600

    Added DBEntityManager for hadling Person and Instructor related DB Operations

---
+++ CSOutreach/DataOperations/DBEntityManager/PersonDBManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataOperations.DBEntityManager
+{
+    public class PersonDBManager
+    {
+     
+    }
+}
