391b91a1e5642f1833db0610a512096d6cb34e54
subbramanil <sxl146830@utdallas.edu>
2014-11-24 12:23:12 -0600

    Committing Authentication.cs file
    Committing Authentication.cs file, changes made by Jonathan

--- CSOutreach/CSOutreach/Authentication.cs
+++ CSOutreach/CSOutreach/Authentication.cs
@@ -1,16 +1,28 @@
-﻿using DataOperations.DBEntity;
+﻿﻿using DataOperations.DBEntity;
 using DataOperations.DBEntityManager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace CSOutreach
 {
     public class Authentication
     {
-        private static bool isvalidusername;
-        private static bool isvalidpassword;
+        private static bool isvalidusername = true;
+        private static bool isvalidpassword = true;
+        private static string attemptedLoginUsername = String.Empty;
+
+        public static void reset()
+        {
+            isvalidusername = true;
+            isvalidpassword = true;
+            attemptedLoginUsername = String.Empty;
+        }
+
+
         public enum Role
         {
             USER,
@@ -24,7 +36,9 @@ namespace CSOutreach
             USERNAME,
             ROLE
         }
-
+        /// <summary>
+        /// Returns true if a user of any role is logged in.
+        /// </summary>
         public static bool Authenticated
         {
             get
@@ -36,12 +50,14 @@ namespace CSOutreach
                 return false;
             }
         }
-
+        /// <summary>
+        /// Retrieves the username (email) from the Session.
+        /// </summary>
         public static string Username
         {
             get
             {
-                string username = (string) HttpContext.Current.Session[SessionVariable.USERNAME.ToString()];
+                string username = (string)HttpContext.Current.Session[SessionVariable.USERNAME.ToString()];
                 if (username != null)
                 {
                     return username;
@@ -59,86 +75,80 @@ namespace CSOutreach
             get { return isvalidpassword; }
         }
 
-        public static bool hasRequiredRole(Role role) 
+        public static string AttemptedLoginUsername
+        {
+            get { return attemptedLoginUsername; }
+        }
+
+        public static bool hasRequiredRole(Role role)
         {
             // TODO: Add real checking.
             return true;
         }
 
-
+        /// <summary>
+        /// Attempt login with user credentials.
+        /// Side Effects: if the login fails, IsValidUsername, AttemptedLoginUsername, IsValidPassword are set to
+        /// indicate the reasons for failure.
+        /// </summary>
+        /// <param name="username">Email address (used as username) of the user</param>
+        /// <param name="password">Password entered by the user</param>
+        /// <returns>true if successful, false if not</returns>
         public static bool login(string username, string password)
         {
-            //TODO: Add real code (or redirect) to log in or log out as appropriate.
-            if (HttpContext.Current.Session[Authentication.SessionVariable.USERNAME.ToString()] == null)
-            {
-                if (Authentication.isValidPerson(username, password))
-                {
-                      HttpContext.Current.Session[Authentication.SessionVariable.USERNAME.ToString()] = username;
-                      return true;
-                }
-            }
-            return false;
-        }
-
-        public static void logout()
-        {
-            // TODO: add real code to sign out
-            HttpContext.Current.Session[Authentication.SessionVariable.USERNAME.ToString()] = null;
-        }
-
-        public static bool isValidPerson(string username, string password)
-        {
             PersonDBManager personDBManager = new PersonDBManager();
-            try
+            Person user = personDBManager.GetUser(username);
+
+
+            if (user == null) // user == null
             {
-                Person person = personDBManager.GetUser(username);
-                if (person != null)
+                isvalidusername = false;
+            }
+            else
+            {
+                isvalidusername = true;
+                attemptedLoginUsername = user.Email;
+
+                if (matchingPasswords(password, user.Password))
                 {
-                    isvalidusername = true;
-                    if (Authentication.isCorrectPassword(password, person.Password))
-                    {
-                        HttpContext.Current.Session["error_message"] += "<br />Incorrect Password.";
-                        isvalidpassword = true; 
-                        return true; 
-                    }
-                    else
-                    { isvalidpassword = false; }
+                    isvalidpassword = true;
+                    HttpContext.Current.Session[Authentication.SessionVariable.USERNAME.ToString()] = user.Email;
                 }
                 else
                 {
-                    isvalidusername = false;
+                    isvalidpassword = false;
+                    HttpContext.Current.Session["error_message"] += "<br />User name doesn't exist.";
                 }
             }
-            catch (Exception ex)
-            { }
 
-            HttpContext.Current.Session["error_message"] += "<br />Null person.";
-
-            return false;
-        }
-
-        public static bool isValidUsername(string username)
-        {
-            PersonDBManager personDBManager = new PersonDBManager();
-            try
+            if (isvalidusername && isvalidpassword)
             {
-                if (personDBManager.GetUser(username) != null)
-                    return true;
+                return true;
             }
-            catch(Exception ex)
-            { }
-             
             return false;
         }
-
-        public static bool isCorrectPassword(string inputPassword, string personPassword)
+        /// <summary>
+        /// If a user is logged in, log them out. This is done simply by setting the user session variable to null.
+        /// Any pages that require users to be logged in check the session for username and redirect if null.
+        /// </summary>
+        public static void logout()
         {
-            //TODO: hash the password before comparing it. 
-            // (since dummy data is currently not hashed.
-            string hashedPassword = inputPassword;
-
-            return true ? inputPassword == personPassword : false;
+            HttpContext.Current.Session[Authentication.SessionVariable.USERNAME.ToString()] = null;
         }
+        /// <summary>
+        /// Compare an unhashed password (input) to hashed password from person object (person.password)
+        /// </summary>
+        /// <param name="inputPassword">password obtained from input</param>
+        /// <param name="personPassword">password obtained form person object</param>
+        /// <returns>true if matching, false if not</returns>
+        public static bool matchingPasswords(string inputPassword, string personPassword)
+        {
+            PersonDBManager dbManager = new PersonDBManager();
+            // Encrypted password in "hashedPassword"
+            string hashedPassword = dbManager.Encrypt(inputPassword);
+            return true ? hashedPassword == personPassword : false;
+        }
+
 
     }
 }
