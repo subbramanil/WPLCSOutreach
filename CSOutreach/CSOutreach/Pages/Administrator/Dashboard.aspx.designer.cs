0a29c88b3daed4a39ec924a827dc7803a3ddfd85
Apurva Gokani <apurvagokani@gmail.com>
2014-11-24 00:14:29 -0600

    Commit for Admin module
    Admin folder was missing....

--- CSOutreach/CSOutreach/Pages/Administrator/Dashboard.aspx.designer.cs
+++ CSOutreach/CSOutreach/Pages/Administrator/Dashboard.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace CSOutreach.Pages.Administrator {
     
     
     public partial class Dashboard {
-        
-        /// <summary>
-        /// RadioButton1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton1;
     }
 }
