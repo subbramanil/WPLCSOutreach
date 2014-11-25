863e36beb258d020734aee74388aa81b47371cea
akashgosi <gosi.akash@gmail.com>
2014-11-25 17:47:45 -0600

    Admin Master Page Changes
    Corrected the links and tree nav style

--- CSOutreach/CSOutreach/Pages/MasterPages/AdministratorMasterpage.master.designer.cs
+++ CSOutreach/CSOutreach/Pages/MasterPages/AdministratorMasterpage.master.designer.cs
@@ -13,6 +13,15 @@ namespace CSOutreach.Pages.MasterPages {
     public partial class AdministratorMasterpage {
         
         /// <summary>
+        /// HeaderContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeaderContent;
+        
+        /// <summary>
         /// TreeView1 control.
         /// </summary>
         /// <remarks>
