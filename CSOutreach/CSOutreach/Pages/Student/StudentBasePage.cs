1d6545ee7f5186bd4ff34d609dfafb35bc4350ec
Aron Sajan Philip <aronsajan@hotmail.com>
2014-12-04 10:35:10 -0600

    StudentBasePage update
    Added more common properties of student in base page

--- CSOutreach/CSOutreach/Pages/Student/StudentBasePage.cs
+++ CSOutreach/CSOutreach/Pages/Student/StudentBasePage.cs
@@ -1,31 +1,34 @@
 ﻿//This page holds the authentication of all student pages.
 
+using DataOperations.DBEntity;
+using DataOperations.DBEntityManager;
 using StudentEntity.PageTraversal;
 using StudentEntity.Session;
 using System;
 using System.Collections.Generic;
+using System.Data.Objects.DataClasses;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 
 namespace CSOutreach.Pages.Student
 {
-    public class StudentBasePage:Page
+    public class StudentBasePage : Page
     {
 
         protected override void OnInit(EventArgs e)
         {
-         if(!AuthenticateUser())
-         {
-             LogOffUser();
-         }
+            if (!AuthenticateUser())
+            {
+                LogOffUser();
+            }
         }
 
         private bool AuthenticateUser()
         {
-            if(Session["USERNAME"]!=null)
+            if (Session["USERNAME"] != null)
             {
-                if(Session["ROLE"].ToString().CompareTo("Student")==0)
+                if (Session["ROLE"].ToString().CompareTo("Student") == 0)
                 {
                     return true;
                 }
@@ -52,11 +55,62 @@ namespace CSOutreach.Pages.Student
         {
             get
             {
-               return Session[StudentSession.CROSS_PAGE_INFORMATION] as object;
+                return Session[StudentSession.CROSS_PAGE_INFORMATION] as object;
             }
             set
             {
                 Session[StudentSession.CROSS_PAGE_INFORMATION] = value;
+            }
+        }
+
+        protected Person StudentDetails
+        {
+            get
+            {
+                string ID = Session[Authentication.SessionVariable.USERNAME.ToString()].ToString();
+                PersonDBManager StudentPerson = new PersonDBManager();
+                return StudentPerson.GetUser(ID);
+            }
+        }
+
+        protected EntityCollection<StudentEvent> StudentEvents
+        {
+            get
+            {
+               return this.StudentDetails.Student.StudentEvents;
+
+            }
+        }
+
+        protected EntityCollection<Course> StudentCourses
+        {
+            get
+            {
+                EntityCollection<Course> AllStudentCourses=new EntityCollection<Course>();
+                foreach(StudentEvent Event in StudentEvents)
+                {
+                    if (!AllStudentCourses.Contains(Event.Event.Course))
+                    {
+                        AllStudentCourses.Add(Event.Event.Course);
+                    }
+                }
+                return AllStudentCourses;
+            }
+        }
+
+        protected int StudentLevel
+        {
+            get
+            {
+                int MaxCourseLevel = -1;
+                foreach (Course CourseEntity in StudentCourses)
+                {
+                   if(CourseEntity.CourseLevel>MaxCourseLevel)
+                   {
+                       MaxCourseLevel = CourseEntity.CourseLevel;
+                   }
+                }
+                return MaxCourseLevel;
             }
         }
 
