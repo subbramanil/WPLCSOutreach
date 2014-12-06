d725253e252e98c6cac387dc2d703ccaf01678af
urvilshah07 <urvil.d.col@gmail.com>
2014-12-06 09:36:37 -0600

    Change password
    change password in profile page

---
+++ CSOutreach/CSOutreach/Pages/Student/Changepassword.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Changepassword.aspx.cs" Inherits="CSOutreach.Pages.Student.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Current password" Width="120px"></asp:Label>
+        <asp:TextBox ID="txt_cpassword" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server"
+            ControlToValidate="txt_cpassword"
+            ErrorMessage="Please enter Current password"></asp:RequiredFieldValidator>
+        <br />
+         <asp:Label ID="Label2" runat="server" Text="New password" Width="120px"
+            Font-Bold="True"></asp:Label>
+        <asp:TextBox ID="txt_npassword" runat="server" TextMode="Password"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server"
+            ControlToValidate="txt_npassword" ErrorMessage="Please enter New password"></asp:RequiredFieldValidator>
+        <br />
+       
+         <asp:Label ID="Label3" runat="server" Text="Confirm password" Width="120px"
+            Font-Bold="True" ></asp:Label>
+
+        <asp:TextBox ID="txt_ccpassword" runat="server" TextMode="Password"></asp:TextBox>   
+
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server"
+            ControlToValidate="txt_ccpassword"
+            ErrorMessage="Please enter Confirm  password"></asp:RequiredFieldValidator>
+
+        <asp:CompareValidator ID="CompareValidator1" runat="server"
+            ControlToCompare="txt_npassword" ControlToValidate="txt_ccpassword"
+            ErrorMessage="Password Mismatch"></asp:CompareValidator>   
+    </div>
+    <asp:Button ID="btn_update" runat="server" Font-Bold="True" BackColor="#CCFF99" onclick="btn_update_Click" Text="Update" />
+    <asp:Label ID="lbl_msg" Font-Bold="True" BackColor="#FFFF66" ForeColor="#FF3300" runat="server" Text=""></asp:Label><br />
+    </form>
+</body>
+</html>
---
+++ CSOutreach/CSOutreach/Pages/Student/Changepassword.aspx.cs
@@ -0,0 +1,70 @@
+﻿using DataOperations.DBEntity;
+using DataOperations.DBEntityManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.ModelBinding;
+using DataOperations.DBEntity;
+using DataOperations.DBEntityManager;
+
+namespace CSOutreach.Pages.Student
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        static string hashvaluepassword, emailId;
+        protected void btn_update_Click(object sender, EventArgs e)
+        {
+            
+            int up = 0;
+            PersonDBManager personDBMgr = new PersonDBManager();
+            try
+            {
+                List<Person> personelist = new List<Person>();
+                using (DBCSEntities entity = new DBCSEntities())
+                {
+                    string userName = CSOutreach.Authentication.Username;
+                    Person person = personDBMgr.GetUser(userName);
+                    hashvaluepassword = person.Password;
+                    emailId = person.Email;
+                    //Password.Value = person.Password;
+                    //CPassword.Value = person.Password;
+
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            if (personDBMgr.Encrypt(txt_cpassword.Text) == hashvaluepassword)
+            {
+                    up = 1;
+            }
+            if (up == 1)
+            {
+                
+                
+                Person password_user = new Person();
+                password_user.Password = txt_npassword.Text;
+                password_user.Email = emailId;
+                bool result = personDBMgr.UpdateUserPassword(password_user);
+                if (result == true)
+                {
+                    lbl_msg.Text = "Password changed Successfully";
+                }
+            }
+            else
+            {
+                lbl_msg.Text = "Please enter correct Current password";
+            }
+        }
+    }
+}
